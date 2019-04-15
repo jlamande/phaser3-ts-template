cf41d58d893045dac41912f1044d4bc05f52e7f7
jlamande@gmail.com <jlamande@gmail.com>
2019-04-15 20:27:19 +0200

    Put sources

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import "phaser";
+import { GameScene } from "./gameScene";
+
+const config: GameConfig = {
+  title: "Starfall",
+  width: 800,
+  height: 600,
+  parent: "game",
+  scene: [GameScene],
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+  backgroundColor: "#18216D"
+};
+
+export class StarfallGame extends Phaser.Game {
+  constructor(config: GameConfig) {
+    super(config);
+  }
+};
+
+window.onload = () => {
+  var game = new StarfallGame(config);
+};
---
+++ src/gameScene.ts
@@ -0,0 +1,23 @@
+import "phaser";
+
+export class GameScene extends Phaser.Scene {
+
+  constructor() {
+    super({
+      key: "GameScene"
+    });
+  }
+
+  init(/*params: any*/): void {
+  }
+
+  preload(): void {
+  }
+
+  create(): void {
+  }
+
+  update(time: number): void {
+  }
+
+};
